eedfb48a90a0f338ee92b5ccb05764bbfe5d768b
ipa3211 <kygsm2003@naver.com>
2023-05-22 15:12:28 +0900

    공통 : PCG에서 쓰던 Websocket, byteUtil 부분 추가

--- DogFightCommon/DogFightCommon/DogFightpacket.cs
+++ DogFightCommon/DogFightCommon/DogFightpacket.cs
@@ -1,10 +1,130 @@
 ﻿using System;
 using UnityEngine;
-using UnityEngine.Rendering;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace DogFightCommon
 {
-    namespace Packet
+    namespace WebSocketPacket
+    {
+        [Serializable]
+        public class WebSocketPacket
+        {
+            [SerializeField] int index;
+            [SerializeField] bool isAns;
+            [SerializeField] int ansTo;
+            [SerializeField] string order;
+
+            public int Index => index;
+            public bool IsAns => isAns;
+            public int AnsTo => ansTo;
+            public string Order => order;
+
+            public WebSocketPacket()
+            {
+                index = this.GetHashCode();
+                order = this.GetType().Name;
+                isAns = false;
+            }
+
+            public void AnswerTo(int ansTo)
+            {
+                isAns = true;
+                this.ansTo = ansTo;
+            }
+        }
+
+        // 네트워크에 요청하면 대답을 기다림 Duration이 지나면 TimeOutException
+        public class NetworkRequest
+        {
+            WebSocketPacket packet;
+            TaskCompletionSource<WebSocketPacket> tsc;
+            float duration;
+
+            public WebSocketPacket Packet => packet;
+
+            public NetworkRequest(WebSocketPacket pack, TaskCompletionSource<WebSocketPacket> inTsc, float dur)
+            {
+                packet = pack;
+                tsc = inTsc;
+                duration = dur;
+            }
+
+            public bool UpdateDuration(float delta)
+            {
+                duration -= delta;
+
+                if (duration < 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public void OnAnswerArrive(WebSocketPacket packet)
+            {
+                tsc.SetResult(packet);
+            }
+
+            public void OnRemove()
+            {
+                TimeoutException e = new TimeoutException();
+                tsc.SetException(e);
+            }
+        }
+    }
+    namespace Util 
+    {
+        public class ByteUtil
+        {
+            public static byte[] ObjectToByteArray(object obj)
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                using (var ms = new MemoryStream())
+                {
+                    bf.Serialize(ms, obj);
+                    return Compress(ms.ToArray());
+                }
+            }
+
+            public static object ByteArrayToObject(byte[] arrBytes)
+            {
+                byte[] decompressByte = Decompress(arrBytes);
+                using (var memStream = new MemoryStream())
+                {
+                    var binForm = new BinaryFormatter();
+                    memStream.Write(decompressByte, 0, decompressByte.Length);
+                    memStream.Seek(0, SeekOrigin.Begin);
+                    var obj = binForm.Deserialize(memStream);
+                    return obj;
+                }
+            }
+
+            public static byte[] Compress(byte[] data)
+            {
+                MemoryStream output = new MemoryStream();
+                using (DeflateStream dstream = new DeflateStream(output, System.IO.Compression.CompressionLevel.Optimal))
+                {
+                    dstream.Write(data, 0, data.Length);
+                }
+                return output.ToArray();
+            }
+
+            public static byte[] Decompress(byte[] data)
+            {
+                MemoryStream input = new MemoryStream(data);
+                MemoryStream output = new MemoryStream();
+                using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+                {
+                    dstream.CopyTo(output);
+                }
+                return output.ToArray();
+            }
+        }
+    }
+    namespace UDPpacket
     {
         [Serializable]
         public class SerializableVector3
