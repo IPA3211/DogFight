3dd421a51e1c81035a589b6bc259a2442d2204b3
ipa3211 <kygsm2003@naver.com>
2023-06-19 15:52:24 +0900

    클라 : Unicast 송신, Multicast 수신 기능 추가

---
+++ Client/Assets/DogFight/Scripts/GameNetworkManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using DogFightCommon.UDPpacket;
+using DogFightCommon.Util;
+
+public class GameNetworkManager : MonoBehaviour
+{
+    UdpClient multi = new UdpClient();
+    IPEndPoint epRemote = new IPEndPoint(IPAddress.Any, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        UdpClient cli = new UdpClient();
+
+        string msg = "æ»≥Á«œººø‰";
+        byte[] datagram = Encoding.UTF8.GetBytes(msg);
+
+        cli.Send(datagram, datagram.Length, "127.0.0.1", 7777);
+
+        IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 5500);
+        multi.Client.Bind(localEp);
+
+        IPAddress multicastIP = IPAddress.Parse("229.1.1.229");
+        multi.JoinMulticastGroup(multicastIP);
+
+
+        // (4) UdpClient ∞¥√º ¥›±‚
+        cli.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        byte[] bytes = multi.Receive(ref epRemote);
+
+        SerializableTransform t = (SerializableTransform)ByteUtil.ByteArrayToObject(bytes);
+        
+        t.OverWriteTransform(transform);
+        Debug.Log(transform.position);
+    }
+}
