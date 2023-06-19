515f1cd3203fae03e65bd4e395f44a2a42775cd3
ipa3211 <kygsm2003@naver.com>
2023-06-19 15:51:45 +0900

    서버 : Udp 수신, multicast 송신, Unicast 송신 기능 추가

--- Server/Assets/DogFight/Scripts/GameNetworkServer.cs
+++ Server/Assets/DogFight/Scripts/GameNetworkServer.cs
@@ -2,26 +2,45 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
+using System.Text;
+using DogFightCommon.UDPpacket;
+using DogFightCommon.Util;
 
 public class GameNetworkServer : MonoBehaviour
 {
-    UdpClient srv = new UdpClient(7777);
-    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+    UdpClient multi = new UdpClient();
+    UdpClient srv;
 
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
+        srv = new UdpClient(7777);
         while (true)
         {
-            byte[] dgram = srv.Receive(ref remoteEP);
-            Console.WriteLine("[Receive] {0} 로부터 {1} 바이트 수신", remoteEP.ToString(), dgram.Length);
-
-            srv.Send(dgram, dgram.Length);
+            var packet = await srv.ReceiveAsync();
+            OnReceiveUdpPacket(packet);
         }
+    }
+
+    void OnReceiveUdpPacket(UdpReceiveResult packet)
+    {
+        Debug.Log(Encoding.UTF8.GetString(packet.Buffer));
+    }
+
+    void SendUdpPacket(byte[] buffer, IPEndPoint endPoint)
+    {
+        srv.Send(buffer, buffer.Length, endPoint);
+    }
+
+    void SendMulticast(byte[] buffer)
+    {
+        IPEndPoint multicastEP = new IPEndPoint(IPAddress.Parse("229.1.1.229"), 5500);
+        multi.Send(buffer, buffer.Length, multicastEP);
     }
 
     void Update()
     {
-        
+        SerializableTransform transform = new SerializableTransform(gameObject.transform);
+        SendMulticast(ByteUtil.ObjectToByteArray(transform));
     }
 }
