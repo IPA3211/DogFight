8bc2da20907d8dcc83c1ee76c80be35de4077022
ipa3211 <kygsm2003@naver.com>
2023-06-20 11:15:50 +0900

    클라 : 서버와 Fighter 코드 동기화

--- Client/Assets/DogFight/Scripts/Fighter.cs
+++ Client/Assets/DogFight/Scripts/Fighter.cs
@@ -1,6 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
+[Serializable]
+public struct FighterInfo
+{
+    [SerializeField] public bool isGearUp;
+    [SerializeField] public bool isXfliped;
+    [SerializeField] public bool isYfliped;
+    [SerializeField] public float pitchPower;
+    [SerializeField] public float speed;
+    [SerializeField] public float fireingInterval;
+    [SerializeField] public float intervalTime;
+}
 
 public class Fighter : MonoBehaviour
 {
@@ -9,16 +23,21 @@ public class Fighter : MonoBehaviour
     [SerializeField] GameObject body;
     [SerializeField] GameObject gun;
     [SerializeField] GameObject gear;
-    [SerializeField] bool isGearUp;
-    [SerializeField] public bool IsXfliped;
-    [SerializeField] public bool IsYfliped;
     Vector2 front => (gun.transform.position - body.transform.position).normalized;
     Vector2 up => (body.transform.position - gear.transform.position).normalized;
-    public float pitchPower = 10f;
-    public float speed = 100f;
-    public float fireingInterval = 1;
-    float intervalTime = 0;
+
+    [SerializeField] FighterInfo info;
+
+    bool isGearUp { get => info.isGearUp; set => info.isGearUp = value; }
+    bool isXfliped { get => info.isXfliped; set => info.isXfliped = value; }
+    bool isYfliped { get => info.isYfliped; set => info.isYfliped = value; }
+    float pitchPower { get => info.pitchPower; set => info.pitchPower = value; }
+    float speed { get => info.speed; set => info.speed = value; }
+    float fireingInterval { get => info.fireingInterval; set => info.fireingInterval = value; }
+    float intervalTime { get => info.intervalTime; set => info.intervalTime = value; }
+
     Rigidbody2D rigid;
+
     Vector3 drag;
     Vector3 lift;
     Vector3 thrust;
@@ -28,8 +47,9 @@ public class Fighter : MonoBehaviour
         Flip();
     }
 
-    void OnDrawGizmos(){
-        
+    void OnDrawGizmos()
+    {
+
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + drag);
         Gizmos.color = Color.blue;
@@ -52,20 +72,20 @@ public class Fighter : MonoBehaviour
 
     public void Flip()
     {
-        fighter.transform.localRotation = Quaternion.Euler(IsYfliped ? 180 : 0, IsXfliped ? 180 : 0, 0);
-        Debug.Log(IsYfliped);
+        fighter.transform.localRotation = Quaternion.Euler(isYfliped ? 180 : 0, isXfliped ? 180 : 0, 0);
+        Debug.Log(isYfliped);
     }
 
     public void Pitch(int pitchDriection)
     {
-        rigid.AddTorque(((IsXfliped && IsYfliped) || (!IsXfliped && !IsYfliped) ? -1 : 1) * pitchPower * pitchDriection * Time.fixedDeltaTime, ForceMode2D.Force);
+        rigid.AddTorque(((isXfliped && isYfliped) || (!isXfliped && !isYfliped) ? -1 : 1) * pitchPower * pitchDriection * Time.fixedDeltaTime, ForceMode2D.Force);
     }
 
     public bool FireBullet()
     {
         if (intervalTime < 0)
         {
-            var newRot = Quaternion.Euler((IsXfliped ? new Vector3(0, 0, 180) : Vector3.zero) + transform.rotation.eulerAngles);
+            var newRot = Quaternion.Euler((isXfliped ? new Vector3(0, 0, 180) : Vector3.zero) + transform.rotation.eulerAngles);
             intervalTime = fireingInterval;
             Instantiate(bullet, transform.position, newRot);
             return true;
@@ -76,9 +96,9 @@ public class Fighter : MonoBehaviour
         }
     }
 
-    public bool RollFighter(bool isFlip)
+    public bool RollFighter()
     {
-        IsYfliped = isFlip;
+        isYfliped = !isYfliped;
         Flip();
         return true;
     }
